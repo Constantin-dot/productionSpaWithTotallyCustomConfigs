edd6e025d5b7571f58e8c5fd524cd06fef2b2d6c
Karelin Konstantin <kkarelin@croc.ru>
2023-02-24 14:04:39 +0300

    #31.1 fixed themeProvider for storybook, added json-server for server imitation

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -2,10 +2,15 @@ import { Story } from '@storybook/react';
 import { Theme, ThemeProvider } from 'app/providers/ThemeProvider';
 import 'app/styles/index.scss';
 
-export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
-  <ThemeProvider initialTheme={theme}>
-    <div className={`app ${theme}`}>
-      <StoryComponent />
-    </div>
-  </ThemeProvider>
-);
+export const ThemeDecorator = (theme: Theme = Theme.LIGHT) => function StoryComponent(
+  StoryComponent: Story,
+) {
+  document.body.className = theme;
+  return (
+    <ThemeProvider>
+      <div className={`app ${theme}`}>
+        <StoryComponent />
+      </div>
+    </ThemeProvider>
+  );
+};
