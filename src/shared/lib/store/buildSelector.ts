831961a343bca2a624828680247d7b6e5c9cef0f
Constantin K. <konstantinkspb@gmail.com>
2023-11-25 17:51:19 +0300

    #124 added arguments for buildSelector

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -1,11 +1,18 @@
 import { useSelector } from 'react-redux';
 import { IStateSchema } from '@/app/providers/StoreProvider';
 
-type SelectorType<T> = (state: IStateSchema) => T;
-type ResultType<T> = [() => T, SelectorType<T>];
+type TSelector<T, TArgs extends any[]> = (
+  state: IStateSchema,
+  ...args: TArgs
+) => T;
+type THook<T, TArgs extends any[]> = (...args: TArgs) => T;
+type TResult<T, TArgs extends any[]> = [THook<T, TArgs>, TSelector<T, TArgs>];
 
-export function buildSelector<T>(selector: SelectorType<T>): ResultType<T> {
-  const useSelectorHook = () => useSelector(selector);
+export function buildSelector<T, TArgs extends any[]>(
+  selector: TSelector<T, TArgs>,
+): TResult<T, TArgs> {
+  const useSelectorHook: THook<T, TArgs> = (...args: TArgs) =>
+    useSelector((state: IStateSchema) => selector(state, ...args));
 
   return [useSelectorHook, selector];
 }
